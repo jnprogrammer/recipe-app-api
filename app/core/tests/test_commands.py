3ef86d2a94b001752474022231aba81e8a60811a
Josh <joshuanparker@gmail.com>
2023-09-26 15:16:49 -0400

    got tests working for db up check

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -4,7 +4,7 @@ Test custom django mangement commands/
 """
 
 from unittest.mock import patch
-from psycopg2 import OperationalError as Psycopg2Error
+from psycopg2 import OperationalError as Psycopg2OpError
 from django.core.management import call_command
 from django.db.utils import OperationalError
 from django.test import SimpleTestCase
@@ -18,17 +18,17 @@ class CommandTests(SimpleTestCase):
 
         call_command('wait_for_db')
 
-        patched_check.assert_called_once_with(database=['default'])
+        patched_check.assert_called_once_with(databases=['default'])
 
     @patch('time.sleep')
     def test_wait_for_db_delay(self, patched_sleep, patched_check):
-        """Test waiting for db when getting op err"""
+        """Test waiting for db when getting OperationalError"""
 
-        patched_check.side_effect = [Psycopg2Error] * 2 + \
+        patched_check.side_effect = [Psycopg2OpError] * 2 + \
             [OperationalError] * 3 + [True]
         
         call_command('wait_for_db')
 
         self.assertEqual(patched_check.call_count, 6)
-        patched_check.assert_called_with(database=[''])
+        patched_check.assert_called_with(databases=['default'])
 
