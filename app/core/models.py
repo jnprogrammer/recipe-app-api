1afa122288a65f35d0e7e230bb0c0345d0009ff1
Josh <joshuanparker@gmail.com>
2023-09-27 14:18:43 -0400

    created a test and custom base model

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,19 @@
-from django.db import models # noqa
+from django.db import models 
 
-# Create your models here.
+"""database models."""
+
+from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin,
+)
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """User in the system."""
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'email'
