f1b62b48344f8e916493dc7a52071fa8cc7a5a64
Josh <joshuanparker@gmail.com>
2023-10-07 11:53:36 -0400

    used mixins to include update ingredient functions

--- app/recipe/tests/test_ingredients_api.py
+++ app/recipe/tests/test_ingredients_api.py
@@ -15,7 +15,7 @@ INGREDIENTS_URL = reverse('recipe:ingredient-list')
 
 def detail_url(ingredient_id):
     """Create and return an ingredient detail URL"""
-    return reverse('recipe:ingredient-detial', args=[ingredient_id])
+    return reverse('recipe:ingredient-detail', args=[ingredient_id])
 
 def create_user(email='user@example.com', password='testpass123'):
     """Create and return user"""
